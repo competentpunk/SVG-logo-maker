f88138b53611aa63b395b24dbcb4daff980e74c7
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-03-29 17:56:44 -0400

    Added base code

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Circle
+<circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
+
+// Triangle 
+<polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
+      stroke="green" fill="transparent" stroke-width="5"/>
+
+// Square
+<rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+
+// inquirer to ask questions (about logo)
+
+// Look up jest js for testing
+
+//  fs to write svg file
