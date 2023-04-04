e83908427e03e1bfaa66e7c89fef5bb167edaca1
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-04-03 22:16:46 -0400

    Added shapes.js code

--- index.js
+++ index.js
@@ -2,49 +2,49 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 // const generateLogo = require("./examples");
 const generateLogo = () => {
-      return "MARKDOWN";
-}
+  return "MARKDOWN";
+};
 const path = require("path");
 
 // write a file that takes the values from inquirer prompts and formats an SVG string
 
 // Inquirer to ask questions (about logo)
 const questions = [
-      {
-        type: "list",
-        name: "color",
-        message: "What color do you want your logo?",
-        choices: ["Blue", "Red", "Yellow"]
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: "What shape?",
-        choices: ["Circle", "Triangle", "Square"]
-      },
+  {
+    type: "list",
+    name: "shape",
+    message: "What shape do you want your logo?",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+
+  {
+    type: "list",
+    name: "shapeColor",
+    message: "Enter HEX code for shape color:",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three characters for logo text:",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter HEX code for text color:",
+  },
 ];
 
-// Circle
-// <circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
-
-// Triangle 
-// <polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
-//       stroke="green" fill="transparent" stroke-width="5"/>
-
-// Square
-// <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
-
 function writeToFile(fileName, data) {
-      return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-};
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // Create file
 function init() {
- inquirer.prompt(questions).then((inquirerResponses) => {
-        writeToFile("Logo.svg", generateLogo({ ...inquirerResponses }));
-      });
-};
-    
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("Logo.svg", generateLogo({ ...inquirerResponses }));
+  })
+  .catch((err) => console.log('Unable to SVG', err));
+}
 // Function call to initialize app
 init();
 
